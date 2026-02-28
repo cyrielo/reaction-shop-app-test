13a63fd8b7271c4d3cc08a89ec1b07b768605208
Cyril Ologho <cyrielo911@gmail.com>
2026-02-27 22:15:49 -0500

    add queryclient provider

--- App.tsx
+++ App.tsx
@@ -6,18 +6,20 @@
  */
 
 import { StatusBar, useColorScheme } from 'react-native';
+import { QueryClientProvider } from '@tanstack/react-query';
 import RootNavigator from './src/navigation/';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { queryClient } from './src/requests/queryClient';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <QueryClientProvider client={queryClient}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppContent />
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
