dd33e50c2a12679708edd5cbca67db368af0c282
Cyril Ologho <cyrielo911@gmail.com>
2026-03-01 02:58:36 -0500

    update App name

--- ios/shopifyProductBrowserApp/AppDelegate.swift
+++ ios/shopifyProductBrowserApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "shopifyProductBrowserApp",
+      withModuleName: "reaction",
       in: window,
       launchOptions: launchOptions
     )
